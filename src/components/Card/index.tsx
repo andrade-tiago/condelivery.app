fa403a408ce7161c2a4140a4f5757e2bcec24caa
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-09-01 18:48:25 -0300

    :sparkles: feat: add card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,106 @@
+import Colors from '@/constants/colors'
+import { View, StyleSheet, Image, Text } from 'react-native'
+import Button from '@/components/Button'
+
+type CardProps = {
+  title: string,
+  subtitle: string, 
+  leftText: string,
+  imgURL?: string,
+  buttons?: {
+    text: string,
+    onClick: Function,
+  }[],
+}
+
+const Card: React.FunctionComponent<CardProps> = (props) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.row}>
+        {props.imgURL && (
+          <View style={styles.imgContainer}>
+            <Image
+              source={{ uri: props.imgURL }}
+              style={styles.img}
+              resizeMode="cover"
+            />
+          </View>
+        )}
+
+        <View style={styles.infoContainer}>
+          <Text style={styles.subtitle}>
+            {props.subtitle}
+          </Text>
+          <Text style={styles.title}>
+            {props.title}
+          </Text>
+        </View>
+
+        <Text style={styles.leftText}>
+          {props.leftText}
+        </Text>
+      </View>
+
+      {props.buttons && (
+        <View style={styles.row}>
+          {props.buttons.map((button, i) => (
+            <Button key={i}
+              variant="full"
+              text={button.text}
+              onClick={button.onClick}
+            />
+          ))}
+        </View>
+      )}
+    </View>
+  )
+}
+export default Card
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    borderRadius: 6,
+    padding: 16,
+    gap: 24,
+    backgroundColor: Colors.white,
+    elevation: 4,
+  },
+  row: {
+    width: '100%',
+    flexDirection: 'row',
+    gap: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imgContainer: {
+    width: 60,
+    height: 60,
+    borderRadius: 6,
+    overflow: 'hidden',
+    elevation: 4,
+  },
+  img: {
+    flex: 1,
+  },
+  infoContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    gap: 4,
+  },
+  subtitle: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: Colors.gray,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: '400',
+    color: Colors.black,
+  },
+  leftText: {
+    fontSize: 16,
+    fontWeight: '400',
+    color: Colors.orange,
+  }
+})
