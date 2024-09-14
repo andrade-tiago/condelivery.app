ae84ef0acbf0025a198ebefa8de724ccde50ac00
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-09-14 15:52:32 -0300

    :wrench: chore: set default image url constants

---
+++ src/constants/images-url.ts
@@ -0,0 +1,6 @@
+const ImagesURLs = {
+  googleLogo: 'https://t.ctcdn.com.br/lvns56iaSMyHvyTur4JeYS_NYeY=/i606944.png',
+  facebookLogo: 'https://static.vecteezy.com/system/resources/previews/018/930/698/non_2x/facebook-logo-facebook-icon-transparent-free-png.png',
+}
+
+export default ImagesURLs
