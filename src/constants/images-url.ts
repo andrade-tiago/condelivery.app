459b23a1959ed87f59162651719d52cfb8672438
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-09-24 19:44:38 -0300

    :truck: chore: move delivery apps images from constants directly to its static content

--- src/constants/images-url.ts
+++ src/constants/images-url.ts
@@ -1,10 +1,6 @@
 const ImagesURLs = {
   googleLogo: 'https://t.ctcdn.com.br/lvns56iaSMyHvyTur4JeYS_NYeY=/i606944.png',
   facebookLogo: 'https://static.vecteezy.com/system/resources/previews/018/930/698/non_2x/facebook-logo-facebook-icon-transparent-free-png.png',
-  ifoodLogo: 'https://i.pinimg.com/564x/4d/03/a4/4d03a42a273fba12fab1004d387c1f1e.jpg',
-  rappiLogo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Rappi_backgr_logo.png/600px-Rappi_backgr_logo.png',
-  zeDeliveryLogo: 'https://courier-images-web.imgix.net/static/img/meta_image.png',
-  dakiLogo: 'https://assets.unileversolutions.com/v1/99522848.png',
 }
 
 export default ImagesURLs
--- src/content/delivery-apps.ts
+++ src/content/delivery-apps.ts
@@ -1,5 +1,3 @@
-import ImagesURLs from "@/constants/images-url"
-
 type DeliveryApp = {
   id: number,
   name: string,
@@ -10,22 +8,22 @@ const deliveryApps: DeliveryApp[] = [
   {
     id: 0,
     name: 'iFood',
-    logoImgURL: ImagesURLs.ifoodLogo,
+    logoImgURL: 'https://i.pinimg.com/564x/4d/03/a4/4d03a42a273fba12fab1004d387c1f1e.jpg',
   },
   {
     id: 1,
     name: 'Rappi',
-    logoImgURL: ImagesURLs.rappiLogo,
+    logoImgURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Rappi_backgr_logo.png/600px-Rappi_backgr_logo.png',
   },
   {
     id: 2,
     name: 'Zé Delivery',
-    logoImgURL: ImagesURLs.zeDeliveryLogo,
+    logoImgURL: 'https://courier-images-web.imgix.net/static/img/meta_image.png',
   },
   {
     id: 3,
     name: 'Daki',
-    logoImgURL: ImagesURLs.dakiLogo,
+    logoImgURL: 'https://assets.unileversolutions.com/v1/99522848.png',
   },
 ]
 
