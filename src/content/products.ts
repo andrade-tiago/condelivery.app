9d363677a978358aca283b387d3a99a040b09253
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-10-13 17:31:15 -0300

    :wrench: chore(products content): add more information

--- src/content/products.ts
+++ src/content/products.ts
@@ -1,28 +1,55 @@
+export type ProductAdditional = {
+  id: number,
+  name: string,
+  price: number,
+}
+
 export type Product = {
+  id: number,
   name: string,
   description: string,
   imgURL: string,
   price: number,
+  additional: ProductAdditional[],
 }
+
+const additional: ProductAdditional[] = [
+  {
+    id: 0,
+    name: 'Mocha',
+    price: 2.5,
+  },
+  {
+    id: 1,
+    name: 'Chantilly',
+    price: 2.5,
+  },
+]
 
 const products: Product[] = [
   {
+    id: 0,
     name: 'Mocha Frappuccino',
     description: 'Café, leite, gelo e calda de mocha, finalizado com chantilly e mocha.',
     imgURL: 'https://www.guiadasemana.com.br/contentFiles/image/opt_w1280h960/2017/09/FEA/55500_frappuccino.jpg',
     price: 29.9,
+    additional: additional,
   },
   {
+    id: 1,
     name: 'Big Mac',
     description: 'Dois hambúrgueres (100% carne bovina), alface americana, queijo processado sabor cheddar, molho especial, cebola, picles e pão com gergelim.',
     imgURL: 'https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcSVCgY4Qbt9Fq6_iTqPg35PFzBhkkHkzfbrPhe0yAgJ8c6ZtQlRwlYsIxIJgLRf9Rwd',
     price: 31.9,
+    additional: [],
   },
   {
+    id: 2,
     name: 'Batata Frita B. Cup',
     description: 'Um cone de batatas fritas estilo belga com cheddar e bacon.',
     imgURL: 'https://gazetadasemana.com.br/images/noticias/125911/28071006_B.Cup-2.jpg.jpg',
     price: 25.5,
+    additional: [],
   },
 ]
 export default products
