22444d532adb1dfb81d45ae0aa34ecfd80200a46
Tiago Andrade <tiagoandradedejoao@gmail.com>
2024-10-24 19:31:14 -0300

    :sparkles: feat(home screen): add active requests preview

--- src/app/(app)/(tabs)/index.tsx
+++ src/app/(app)/(tabs)/index.tsx
@@ -2,16 +2,19 @@ import AddressArea from "@/components/AddressArea"
 import Banner from "@/components/Banner"
 import LoadingScreen from "@/components/LoadingScreen"
 import ProfileCard from "@/components/ProfileCard"
+import RequestCard from "@/components/RequestCard"
 import { H2 } from "@/components/Text"
 import Colors from "@/constants/colors"
 import { DeliveryApp } from "@/content/delivery-apps"
 import useDeliveryApps from "@/hooks/use-delivery-apps"
+import useRequests from "@/hooks/use-requests"
 import { Octicons } from "@expo/vector-icons"
 import { Link } from "expo-router"
-import { FlatList, ListRenderItem, StyleSheet, View } from "react-native"
+import { FlatList, ListRenderItem, ScrollView, StyleSheet, View } from "react-native"
 
 const Home: React.FunctionComponent = () => {
   const deliveryApps = useDeliveryApps()
+  const requests = useRequests()
 
   const bannerImg = 'https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.3224072:1651227530/mc-picanha-divulgacao-1280x630.jpg'
 
@@ -31,11 +34,25 @@ const Home: React.FunctionComponent = () => {
     )
   }
 
-  if (deliveryApps.isLoading) {
+  if (deliveryApps.isLoading || requests.isLoading) {
     return <LoadingScreen />
   }
+
+  const completedRequestsCards = requests.data?.filter(item => !item.completed).map(item => {
+    return (
+      <RequestCard key={item.id}
+        appName={item.app}
+        completed={item.completed}
+        id={item.id}
+        items={item.items}
+        storeImgURL={item.storeImgURL}
+        storeName={item.store}
+      />
+    )
+  })
+
   return (
-    <View style={styles.screen}>
+    <ScrollView style={styles.screen} contentContainerStyle={styles.screenContentContainer}>
       <View style={styles.header}>
         <AddressArea />
 
@@ -51,7 +68,7 @@ const Home: React.FunctionComponent = () => {
         imgURL={bannerImg}
       />
 
-      <View style={styles.deliveryAppsSection}>
+      <View style={styles.section}>
         <H2>
           Apps disponíveis
         </H2>
@@ -64,7 +81,17 @@ const Home: React.FunctionComponent = () => {
           contentContainerStyle={styles.deliveryAppsListInnerContainer}
         />
       </View>
-    </View>
+
+      <View style={styles.section}>
+        <H2>
+          Seus pedidos
+        </H2>
+
+        <View style={styles.requestsList}>
+          {completedRequestsCards}
+        </View>
+      </View>
+    </ScrollView>
   )
 }
 export default Home
@@ -72,8 +99,10 @@ export default Home
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    padding: 16,
     backgroundColor: Colors.white[0],
+  },
+  screenContentContainer: {
+    padding: 16,
     gap: 24,
   },
   header: {
@@ -82,7 +111,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
   },
-  deliveryAppsSection: {
+  section: {
     width: '100%',
     gap: 12,
   },
@@ -91,7 +120,11 @@ const styles = StyleSheet.create({
   },
   deliveryAppsListInnerContainer: {
     minWidth: '100%',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     gap: 24,
+  },
+  requestsList: {
+    width: '100%',
+    gap: 8,
   },
 })
